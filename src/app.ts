df1e036c7cb2027027d55a97278084bdb801f97f
mustapha mohammed <mustafaumar70@live.com>
2020-04-05 12:19:35 +0100

    server

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+
+import express from 'express';
+import bodyParser from "body-parser";
+import mongoose from 'mongoose';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import http from 'http';
+
+
+
+
+//database 
+/**
+ *mongoose.connect(
+  `mongodb+srv://pharm:${process.env.DB_PASSWORD}@cluster0-r8kjr.mongodb.net/test?retryWrites=true&w=majority`,
+  { useNewUrlParser: true },
+).then(() =>   console.log('connected'))
+.catch((error) => console.log(error.toString()));
+
+ */
+const app = express();
+
+
+
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+//server
+const port = process.env.PORT && parseInt(process.env.PORT, 10) || 8081;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, ()=> {
+    console.log("server running.....")
+});
+
+
