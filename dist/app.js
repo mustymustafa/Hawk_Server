2f18d06a7ec50bf7de63d1dbe71a5e47bf177151
mustapha mohammed <mustafaumar70@live.com>
2020-05-11 16:54:56 +0100

    s

--- dist/app.js
+++ dist/app.js
@@ -3,7 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const cors_1 = __importDefault(require("cors"));
