1eae1eeb70104cd7c1e61beddf8ac57c7fc0e59a
mustapha mohammed <mustafaumar70@live.com>
2020-05-11 16:42:09 +0100

    a

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
 import express from 'express';
 import bodyParser from "body-parser";
 import mongoose from 'mongoose';
