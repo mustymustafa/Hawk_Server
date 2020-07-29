3208ba317720ecd50757f0a44d5a4274d16c5d2c
mustapha mohammed <mustafaumar70@live.com>
2020-07-29 10:02:02 +0100

    m

--- src/validator/Validator.ts
+++
@@ -1,8 +0,0 @@
-export default class Validator {
-  static validateEmail (email: string) {
-    if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
-      return true;
-    }
-    return false;
-  }
-}
