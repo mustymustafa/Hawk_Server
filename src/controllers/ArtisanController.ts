096f12dd065f7dd4661c7d74658fb0f6d60403fe
mustapha mohammed <mustafaumar70@live.com>
2020-09-21 21:18:21 +0100

    m

--- src/controllers/ArtisanController.ts
+++ src/controllers/ArtisanController.ts
@@ -1561,7 +1561,7 @@ static async getDriverRegistartion(request: Request, response: Response) {
   //ADMIN ACTIVATE
 
    static async adminActivate(request: Request, response: Response) {
-    const { uid } = request.body
+    const { id } = request.body
     //const expire =  addWeek(new Date(), 1).toLocaleDateString();
     var now = new Date();
 
@@ -1572,10 +1572,10 @@ static async getDriverRegistartion(request: Request, response: Response) {
 
 
     try {
-      const user = await Schema.Artisan().findOne({ _id: uid })
+      const user = await Schema.Artisan().findOne({ _id: id })
       if (user) {
 
-        await Schema.Artisan().updateOne({ _id: uid },
+        await Schema.Artisan().updateOne({ _id: id },
           {
             $set: {
               active: true,
