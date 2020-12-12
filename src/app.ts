9301a1de5f25a97a9a041a48cc22006e9cb9f9f0
musty <mustafaumar70@live.com>
2020-12-12 13:56:54 +0000

    m

--- src/app.ts
+++ src/app.ts
@@ -35,7 +35,7 @@ const app = express();
 
 
 
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -685,7 +685,7 @@ const users = async ()  => {
 }
     })
 }
-users();
+//users();
 
 
 
